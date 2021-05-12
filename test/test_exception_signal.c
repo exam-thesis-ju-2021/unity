6d8458aed13d7d9876ce92487ca701c1e166cf8f
Fredrik Karlsson <f.v.carlsson@gmail.com>
2021-05-12 13:51:28 +0200

    feat: add 'test_exception_signal.c'

---
+++ test/test_exception_signal.c
@@ -0,0 +1,22 @@
+#include "unity.h"
+#include <signal.h>
+
+void setUp(void)
+{
+	
+}
+
+void tearDown(void)
+{
+
+}
+
+void test_exceptions_SIGSEGV(void)
+{
+	raise(SIGSEGV);
+}
+
+void test_exceptions_SIGILL(void)
+{
+	raise(SIGILL);
+}
